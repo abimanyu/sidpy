c44b73715a810d7fa28c108cd82fd6322902ca79
Abimanyu <bima@abimanyu.net>
2020-09-21 11:48:46 +0700

    berita views

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from apps.profil.models import Profil, Kategori
 from apps.berita.models import Berita, Category
+from apps.qapuas.models import site_conf
 
 def frontpage(request):
     list_profil = Profil.objects.all()
     list_kategori = Profil.objects.all()
     list_berita = Berita.objects.filter(is_featured=True)
     list_category = Category.objects.all()
+    list_frontlink = site_conf.objects.filter(conf='frontlink')
     context = {
         'list_profil': list_profil,
         'list_kategori': list_kategori,
         'List_Berita': list_berita,
-        'List_Category': list_category
+        'List_Category': list_category,
+        'conf_frontlink': list_frontlink
     }
     return render(request, 'frontpage.html', context)
+
