a07628d9630bd5d25e2d6070a55cc0831d2b01c2
Abimanyu <bima@abimanyu.net>
2020-10-01 06:01:02 +0700

    auth login_required

--- apps/qapuas/views.py
+++ apps/qapuas/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import site_conf
 
+@login_required
 def dashboard (request):
     nama_desa = site_conf.objects.filter(conf='desa').first()
-
     context = {
         'nama_desa': nama_desa.conf_val
     }
     return render(request, 'qapuas/dashboard.html', context)
-
