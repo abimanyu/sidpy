e981c9f7a5223b43e5513ff0defd8ad862f08c52
Abimanyu <bima@abimanyu.net>
2020-10-01 05:56:47 +0700

    added cke, slugify

--- apps/berita/models.py
+++ apps/berita/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-from martor.models import MartorField
+from django.utils.text import slugify
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 
 class Category(models.Model):
     title = models.CharField(max_length=255)
@@ -14,14 +16,63 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+    def save(self, *args, **kwargs):
+        if self.slug == None:
+            slug = slugify(self.title)
+
+            has_slug = Category.objects.filter(slug=slug).exists()
+            count = 1
+            while has_slug:
+                count += 1
+                slug = slugify(self.title) + '-' + str(count)
+                has_slug = Category.objects.filter(slug=slug).exists()
+            self.slug =slug
+
+        super().save(*args, **kwargs)
+
+# ___________________________________
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if self.slug == None:
+            slug = slugify(self.title)
+            has_slug = Tag.objects.filter(slug=slug).exists()
+            count = 1
+            while has_slug:
+                count += 1
+                slug = slugify(self.title) + '-' + str(count)
+                has_slug = Tag.objects.filter(slug=slug).exists()
+            self.slug =slug
+
+        super().save(*args, **kwargs)
+# ___________________________________
+
 class Berita(models.Model):
     category = models.ForeignKey(Category, related_name='all_berita', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255)
-    isi = MartorField(blank=True, null=True)
+
+    thumbnail = models.ImageField(upload_to='thumb', null=True, blank=True)
+    
+    isi = RichTextUploadingField(blank=True, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date_added = models.DateTimeField(auto_now_add=True)
-    is_featured = models.BooleanField(default=False)
+
+    STATUS = (
+        ('Publish', 'Publish'),
+        ('Draft', 'Draft'),
+        ('InActive', 'InActive')
+    )
+    status = models.CharField(max_length=20, default='Publish', blank=True, choices=STATUS)
+
+    featured = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, default=1)
+    slug = models.SlugField(max_length=255, blank=True, null=True)
 
     class Meta:
         verbose_name_plural = 'Berita'
@@ -29,3 +80,17 @@ class Berita(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        if self.slug == None:
+            slug = slugify(self.title)
+
+            has_slug = Berita.objects.filter(slug=slug).exists()
+            count = 1
+            while has_slug:
+                count += 1
+                slug = slugify(self.title) + '-' + str(count)
+                has_slug = Berita.objects.filter(slug=slug).exists()
+            self.slug =slug
+
+        super().save(*args, **kwargs)
