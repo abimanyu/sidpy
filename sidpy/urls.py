f289180e22a6b4645901e92df875d894a7a6c844
Abimanyu <bima@abimanyu.net>
2020-10-01 06:05:17 +0700

    remove martor, auth url, added cke

--- sidpy/urls.py
+++ sidpy/urls.py
@@ -1,26 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-
-
-
 from apps.qapuas.views import dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('martor/', include('martor.urls')),
 
     path('', include('apps.core.urls')),
     path('', include('apps.profil.urls')),
-    path('', include('apps.berita.urls')),
+    path('', include('apps.berita.urls')),    
 
-
-    
-
-    
-
-    path('landing/', dashboard, name='dashboard'),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('landing', auth_views.LoginView.as_view(template_name='qapuas/auth_login.html'), name='login'),
+    path('logout', auth_views.LogoutView.as_view(template_name='qapuas/auth_logout.html'), name='logout'),
+    path('ckeditor/', include('ckeditor_uploader.urls'))
 ]
 
 if settings.DEBUG:
