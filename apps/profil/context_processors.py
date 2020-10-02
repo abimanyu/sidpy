9e90a5a55021a09e09e7e782edfbcc84f19c3df4
Abimanyu <bima@abimanyu.net>
2020-10-02 19:35:27 +0700

    nav menu processors

--- apps/profil/context_processors.py
+++ apps/profil/context_processors.py
@@ -1,13 +1,13 @@
 from .models import Category, Pages
 
 def menu_pages_category(request):
-    category_list = Category.objects.all()
+    category_list = Category.objects.all().order_by('id')
     return {'menu_pages_category': category_list}
 
 def menu_pages(request):
-    pages_umum = Pages.objects.filter(category_id='1')
+    pages_umum = Pages.objects.filter(category_id='1').order_by('ordering')
     return {'menu_pages_umum': pages_umum}
 
 def menu_pages_ppid(request):
-    ppid_list = Pages.objects.filter(category_id='3')
+    ppid_list = Pages.objects.filter(category_id='3').order_by('ordering')
     return {'menu_pages_ppid': ppid_list}
